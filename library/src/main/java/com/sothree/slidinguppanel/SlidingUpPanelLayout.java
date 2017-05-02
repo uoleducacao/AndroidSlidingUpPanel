95d47ed3fb6ec786614b0e81f79e3db748d0cf5d
Alexandre Alves <alexandredias1995@gmail.com>
2017-05-02 10:57:42 -0300

    Update SlidingUpPanelLayout.java

--- library/src/main/java/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
+++ library/src/main/java/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
@@ -729,11 +729,11 @@ public class SlidingUpPanelLayout extends ViewGroup {
         final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
         final int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
-        if (widthMode != MeasureSpec.EXACTLY && widthMode != MeasureSpec.AT_MOST) {
+        /*if (widthMode != MeasureSpec.EXACTLY && widthMode != MeasureSpec.AT_MOST) {
             throw new IllegalStateException("Width must have an exact value or MATCH_PARENT");
         } else if (heightMode != MeasureSpec.EXACTLY && heightMode != MeasureSpec.AT_MOST) {
             throw new IllegalStateException("Height must have an exact value or MATCH_PARENT");
-        }
+        }*/
 
         final int childCount = getChildCount();
 
